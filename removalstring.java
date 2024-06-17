a6ce8b93eab8f0cea8e5236dfab504d342594960
giftsonsolomon <giftsonsolomon7@gmail.com>
2024-06-17 20:09:49 +0530

    fix some errors

--- removalstring.java
+++ removalstring.java
@@ -5,22 +5,22 @@ public static void main(String args[])throws IOException
 {
 String str,sub;
 BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-System.out.println(“Enter the string”);
+System.out.println("Enter the string");
 str=br.readLine();
-System.out.println(“Enter the substring”);
+System.out.println("Enter the substring");
 sub=br.readLine();
 StringBuffer str1=new StringBuffer(str);
 int l1,l2,l=0,i=0,flag=0;
 l1=str1.length();
 l2=sub.length();
-if((l2>l1)||(l2<=0))
-System.out.println(“Invalid substring”);
+if((l2>l1)||(l2<=0));
+System.out.println("Invalid substring");
 else
 {
 do
 {
 String s=str1.substring(i,i+l2);
-if(s.equals(sub))
+if(s.equals(sub));
 {
 str1.delete(i,i+l2);
 flag=1;
@@ -30,9 +30,9 @@ i++;
 l=str1.length();
 }
 while((l-i)>=l2);
-if(flag==0)
-System.out.println(“No string”);
-else
-System.out.println(“New string”+str1);
+if(flag==0);
+System.out.println("No string");
+else;
+System.out.println("New string"+str1);
 }
 }}
