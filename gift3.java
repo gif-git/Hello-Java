7cd0e65408163d9cbf933d8b1b5d21d5de4d30a7
giftsonsolomon <giftsonsolomon7@gmail.com>
2024-06-20 18:01:08 +0530

    java addtion program added

---
+++ gift3.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class gift3
+{
+public static void main (String args[])
+{
+int a,b,c;
+a=Integer.parseInt(args[0]);
+b=Integer.parseInt(args[1]);
+c=a+b;
+System.out.println("The out put is:" + c);
+}
+}
